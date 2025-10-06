f665d9cbaf490ded1967b5ef0372dc2e224a0323
Hxrsh01 <harshkrgupta2605@gmail.com>
2025-10-07 00:47:05 +0530

    firstpage

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,16 +1,27 @@
 import React from 'react'
 
 const Welcome = () => {
+  const name = "WELCOME TO THE NEW WORLD"
   const style = {
-    background: "red",
+    background: "linear-gradient(to bottom, #0f2027, #203a43, #2c5364)",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    height:"100vh"
+    height: "100vh"
+  };
+  const btn = {
+    height: "100px",
+    width: "200px",
+    cursor: "pointer",
+    background: "black",
+    color: "white",
+    borderRadius: "10%",
+    border: "2px solid grey"
   };
   return (
-    <div style={style}>
-      <h1>WELCOME TO THE NEW WORLD</h1>
+    <div style={style} className='container'>
+      {/* <button style={btn}>WELCOME</button> */}
+      <h1 >{name}</h1>
     </div>
   )
 }
