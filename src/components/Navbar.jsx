8f9b871ed5397f5e24df92c0b2c77a2225915404
kushika0 <kushika1404@gmail.com>
2025-11-15 01:05:58 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Navbar = () => {
-  const style={
-    Height:"20%",
-    backgroundColor:"red",
-    width:"100%",
-    display:"flex",
-    justifyContent:"center",
-    alignItems:"center",
-    hover:"black"
-  }
-  return (
-    <> 
-    <nav style={style}>
-        <h1>Logo</h1>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Service</li>
-        </ul>
-      </div>
-    </nav>
-    </>
-  )
-}
+  const [open, setOpen] = useState(false);
 
-export default Navbar
+  return (
+    <nav className="w-full bg-purple-200  h-20 flex items-center  justify-between ">
+      
+     
+      <img 
+        src="/logo.png"    
+        alt="logo"
+        className="h-10 w-25"
+      />
+
+     
+      <ul className="hidden md:flex gap-16 text-black-400 text-lg ">
+        <li className="hover:text-black  hover:scale-110  hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">Home</li>
+        <li className="hover:text-black  hover:scale-110  hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">About</li>
+        <li className="hover:text-black  hover:scale-110  hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">Contact</li>
+        <li className="hover:text-black  hover:scale-110  hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm  ">Service</li>
+      </ul>
+
+      
+      <button 
+        className="md:hidden text-black text-3xl"
+        onClick={() => setOpen(!open)}
+      >
+        ☰
+      </button>
+
+      
+      {open && (
+        <ul className="absolute top-20 left-0 w-full bg-purple-300 flex flex-col items-center gap-6 py-6 text-black text-lg md:hidden">
+          <li className="hover:text-black  hover:scale-110  hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">Home</li>
+          <li className="hover:text-black  hover:scale-110  hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">About</li>
+          <li className="hover:text-black hover:scale-110   hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">Contact</li>
+          <li className="hover:text-black hover:scale-110   hover:underline cursor-pointer  bg-gray-400 h-7 w-15 text-center rounded-sm ">Service</li>
+        </ul>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
